5d24495d8075ee7556da0e2b05b126ab1f58d777
ram <justforthegreatergood@gmail.com>
2025-01-24 18:21:51 +0800

    changed classifer to be used in cpu

--- app.py
+++ app.py
@@ -139,7 +139,7 @@ def main():
     # Initialize classifier
     classifier = HandLandmarkClassifier(
         landmark_model_path="models/hand_landmarker.task",
-        classifier_model_path="models/hand_keypoints_classifier_new.pt"
+        classifier_model_path="models/hand_keypoints_classifier_new_cpu.pt"
     )
     
     # Threading setup
